2389fb399fb49e5ef26508eee0bee9962d5d86d7
buschidos <buschidos@buschidos.local>
2014-10-13 13:52:46 +0300

    almost done

--- js/common.js
+++ js/common.js
@@ -1,13 +1,58 @@
 head.ready(function() {
 
-	// $(document).on("click", function(){
-	// 	$(".js-popup").hide();
-	// });
+	$('.fancybox').fancybox();
 
-    $(".fancybox").fancybox();
+	var fullpage_el = $('#fullpage');
+	if (fullpage_el.length) {
+		$('#fullpage').fullpage({
+			onLeave: function(index, nextIndex, direction){
+				//after leaving section 1
+				if(index == '1' && direction =='down'){
+					setTimeout(function(){
+						$('.parallax').addClass('is-scroll-down');
+					}, 250);
+				};
+				if(index == '2' && direction =='up'){
+					setTimeout(function(){
+						$('.parallax').removeClass('is-scroll-down');
+					}, 350);
+				};
+			}
+		});
+	};
 
-    var fullpage_el = $('#fullpage');
-    if (fullpage_el.length) {
-    	$('#fullpage').fullpage();
-    };
+	// popup
+	$('.js-popup-open').on('click', function(event) {
+		event.preventDefault();
+		var popup  = $('#'+ $(this).attr('href'));
+		var popupContent = popup.find('.js-popup-content');
+		var popupClose   = popup.find('.js-popup-close');
+
+		popup.fadeIn(200);
+		setTimeout( function(){
+			popupContent.addClass('is-visible');
+		}, 200);
+	});
+
+	$('.js-popup-close').on('click', function(event) {
+		event.preventDefault();
+		var popupContent = $(this).parents('.js-popup-content')
+		var popup = $(this).parents('.js-popup');
+		popupContent.removeClass('is-visible');
+		setTimeout(function(){
+			popup.fadeOut(200);
+		}, 200);
+	});
+
+	$('.js-popup').on('click', function(event) {
+		event.stopPropagation();
+	});
+
+
+	// gallery sort
+	$('.gallery__sort').on('click', function(event) {
+		event.preventDefault();
+		$('.gallery__sort-more').slideToggle();
+	});
+
 });
