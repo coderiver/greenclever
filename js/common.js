5468da2f1df3998eeb28400929e50ead5defe480
buschidos <sergey.snozyk@gmail.com>
2014-10-14 13:25:42 +0300

    optimization for small screens

--- js/common.js
+++ js/common.js
@@ -5,21 +5,29 @@ head.ready(function() {
 	var fullpage_el = $('#fullpage');
 	if (fullpage_el.length) {
 		$('#fullpage').fullpage({
+			//settings
+			scrollingSpeed: 1000,
+			//events
 			onLeave: function(index, nextIndex, direction){
 				//after leaving section 1
 				if(index == '1' && direction =='down'){
 					setTimeout(function(){
 						$('.parallax').addClass('is-scroll-down');
-					}, 250);
+					}, 300);
 				};
 				if(index == '2' && direction =='up'){
 					setTimeout(function(){
 						$('.parallax').removeClass('is-scroll-down');
-					}, 350);
+					}, 330);
 				};
 			}
 		});
 	};
+
+	$('.js-to-second-section').on('click', function(event) {
+		event.preventDefault();
+		$.fn.fullpage.moveTo(2);
+	});
 
 	// popup
 	$('.js-popup-open').on('click', function(event) {
